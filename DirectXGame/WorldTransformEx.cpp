9ffc637a221aa05b06cdb1b583d8d7f854f94719
KeroppiKun <fuga0809ace@gmail.com>
2023-04-21 14:41:51 +0900

    行列更新関数追加

---
+++ DirectXGame/WorldTransformEx.cpp
@@ -0,0 +1,7 @@
+#include "WorldTransform.h"
+#include "Matrix4x4.h"
+
+void WorldTransform::UpdateMatrix() {
+	matWorld_ = Matrix4x4::MakeAffineMatrix(scale_, rotation_, translation_);
+	TransferMatrix();
+}
